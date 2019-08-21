73a4465649591ed1bf0a098ed6a214d35921568a
Sridatta <sridatta.pasumarthy@oracle.com>
2019-08-21 10:28:13 +0530

    Second commit

---
+++ build/precache-manifest.9057f50e8cdc474fdb90b70452b366aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7713da76c083bf737c731b3499b89b2c",
+    "url": "/static/media/Algolia-logo.7713da76.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5524eb057896f62b3049",
+    "url": "/static/js/main.5524eb05.chunk.js"
+  },
+  {
+    "revision": "33c27ea4d56a6cb99c2b",
+    "url": "/static/js/1.33c27ea4.chunk.js"
+  },
+  {
+    "revision": "5524eb057896f62b3049",
+    "url": "/static/css/main.34d36515.chunk.css"
+  },
+  {
+    "revision": "6a0f460ec754193877cae6a53ef239a7",
+    "url": "/index.html"
+  }
+];
